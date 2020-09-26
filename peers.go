dc32c6e6fad75b9784deb892d2c82c7f7c23c2fd
hoslo <ys952572463@163.com>
2020-09-26 15:14:31 +0800

    first

---
+++ peers.go
@@ -0,0 +1,12 @@
+package geecache
+
+import pb "geecache/geecachepb"
+
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer.
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
